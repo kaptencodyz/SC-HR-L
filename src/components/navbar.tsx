6c7797a89785f0d8cea74443dda6d8955cd9416f
Emil Svedrin <emil.svedrin@gmail.com>
2023-10-10 21:58:27 +0200

    uppdated styling for the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,10 +8,45 @@ const NavBar = () => {
       </div>
       <div className="flex flex-row w-full justify-center">
         <div className="h-10 w-2/5 mt-2 flex justify-between">
-          <Link className="bg-contrast w-1/5 flex justify-center items-center" href="">Job list</Link>
-          <Link className="bg-contrast w-1/5" href="">Enlisted jobs</Link>
-          <Link className="bg-contrast w-1/5" href="">Create Job</Link>
-          <Link className="bg-contrast w-1/5" href="">Support</Link>
+          <Link className="w-1/5 rounded-tl-2xl rounded-tr-2xl p-0.5 pb-0 bg-gradient-to-b from-contrast/70 to-primary" href="">
+            <div className="bg-primary w-full h-full flex justify-center items-bottom rounded-tl-2xl rounded-tr-2xl">
+              <div className="w-full h-full flex justify-center rounded-tl-2xl rounded-tr-2xl bg-gradient-to-b from-contrast/30 to-primary">
+                <p className="flex items-center text-textColor">
+                  Job list
+                </p>
+              </div>
+            </div>
+          </Link>
+
+          <Link className="w-1/5 rounded-tl-2xl rounded-tr-2xl p-0.5 pb-0 hover:bg-gradient-to-b from-contrast/70 to-primary" href="">
+            <div className="bg-primary w-full h-full flex justify-center items-bottom rounded-tl-2xl rounded-tr-2xl">
+              <div className="w-full h-full flex justify-center rounded-tl-2xl rounded-tr-2xl active:bg-gradient-to-b from-contrast/30 to-primary">
+                <p className="flex items-center text-textColor">
+                  Enlisted jobs
+                </p>
+              </div>
+            </div>
+          </Link>
+
+          <Link className="w-1/5 rounded-tl-2xl rounded-tr-2xl p-0.5 pb-0 hover:bg-gradient-to-b from-contrast/70 to-primary" href="">
+            <div className="bg-primary w-full h-full flex justify-center items-bottom rounded-tl-2xl rounded-tr-2xl">
+              <div className="w-full h-full flex justify-center rounded-tl-2xl rounded-tr-2xl active:bg-gradient-to-b from-contrast/30 to-primary">
+                <p className="flex items-center text-textColor">
+                  Create Job
+                </p>
+              </div>
+            </div>
+          </Link>
+
+          <Link className="w-1/5 rounded-tl-2xl rounded-tr-2xl p-0.5 pb-0 hover:bg-gradient-to-b from-contrast/70 to-primary" href="">
+            <div className="bg-primary w-full h-full flex justify-center items-bottom rounded-tl-2xl rounded-tr-2xl">
+              <div className="w-full h-full flex justify-center rounded-tl-2xl rounded-tr-2xl active:bg-gradient-to-b from-contrast/30 to-primary">
+                <p className="flex items-center text-textColor">
+                  Support
+                </p>
+              </div>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
